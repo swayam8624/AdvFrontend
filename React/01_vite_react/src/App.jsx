9377a04fd0a414e484a6137df19f892b13bbcac8
swayam singal <swayam.singal@gmail.com>
2025-01-08 11:43:47 +0530

    fragment+conv

--- React/01_vite_react/src/App.jsx
+++ React/01_vite_react/src/App.jsx
@@ -1,7 +1,8 @@
+import Chai from "./chai"
 
 function App() {
   return (
-      <h1>Hello World</h1>
+      <Chai/>
   )
 }
 
---
+++ React/01_vite_react/src/chai.jsx
@@ -0,0 +1,11 @@
+function Chai() {
+    return (
+        <>
+            <h3>chai is ready</h3>
+            <p>HAHAHA</p>
+        </> // called a fragment
+        
+    );
+}
+
+export default Chai
